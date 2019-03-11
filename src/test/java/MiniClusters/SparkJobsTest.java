a1fe7a521947df13b1329db782a460d3a91c35e8
Mohamed ZOUAOUI <mzouaoui@finaxys.com>
2019-03-11 17:16:26 +0100

    Test

--- src/test/java/MiniClusters/SparkJobsTest.java
+++ src/test/java/MiniClusters/SparkJobsTest.java
@@ -1,20 +1,25 @@
 package MiniClusters;
 
+import java.io.IOException;
 import org.apache.spark.sql.SparkSession;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 public class SparkJobsTest {
 
     private SparkSession spark;
+    @Rule
+    public TemporaryFolder folder= new TemporaryFolder();
 
     @Before
-    public void setUp() {
+    public void setUp() throws IOException {
         spark = SparkSession
                 .builder()
                 .master("local[4]")
                 .appName("Java Spark Hive Example")
-                .config("spark.sql.warehouse.dir", "./target/warehouse")
+                .config("spark.sql.warehouse.dir", folder.newFolder().getAbsolutePath())
                 .config("spark.ui.enabled", "false")
                 .config("spark.driver.allowMultipleContexts", "true")
                 .config("spark.driver.port", 50500)
